034d2a85c0859acd4379f3737b2161da3e6c60c4
3wasfnjd <44668762+3wasfnjd@users.noreply.github.com>
2026-09-18 03:48:53 +0300

    Reduce AR car top speed for arena driving

--- js/Vehicle.js
+++ js/Vehicle.js
@@ -8,7 +8,7 @@ const temp = new THREE.Vector3();
 const targetQuat = new THREE.Quaternion();
 
 const LINEAR_DAMP = 0.1;
-const MAX_SPEED = 1.55;
+const MAX_SPEED = 0.95;
 const REVERSE_SPEED_SCALE = 0.6;
 
 function lerpAngle(a, b, t) {
